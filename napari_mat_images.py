75df04438105ab250560475313e029d1def2173f
Hector <hectormz.git@gmail.com>
2020-03-16 16:08:34 -0700

    Initial ability to read .mat files with tests

--- napari_mat_images.py
+++ napari_mat_images.py
@@ -1,32 +1,18 @@
 """
-This module is an example of a barebones plugin, using imageio.imread.
-
-It implements the ``napari_get_reader`` hook specification, (to create
-a reader plugin) but your plugin may choose to implement any of the hook
-specifications offered by napari.
-
-Type annotations here are OPTIONAL!
-If you don't care to annotate the return types of your functions
-your plugin doesn't need to import, or even depend on napari at all!
-
-Replace code below accordingly.
+This module is a plugin to read images from .mat files in napari
 """
-# for optional type hints only, otherwise you can delete/ignore this stuff
 from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple, Union
 
-import imageio
 import numpy as np
+import scipy.io as sio
 from pluggy import HookimplMarker
 
 LayerData = Union[Tuple[Any], Tuple[Any, Dict], Tuple[Any, Dict, str]]
 PathLike = Union[str, List[str]]
 ReaderFunction = Callable[[PathLike], List[LayerData]]
-# END type hint stuff.
 
 napari_hook_implementation = HookimplMarker("napari")
-IMAGEIO_EXTENSIONS = tuple(
-    set(x for f in imageio.formats for x in f.extensions)
-)
+MAT_EXTENSIONS = '.mat'
 
 
 @napari_hook_implementation
@@ -37,22 +23,73 @@ def napari_get_reader(path: PathLike) -> Optional[ReaderFunction]:
         # if it is a list, it is assumed to be an image stack...
         # so we are only going to look at the first file.
         path = path[0]
-    if not path.endswith(IMAGEIO_EXTENSIONS):
+    if not path.endswith(MAT_EXTENSIONS):
         # if we know we cannot read the file, we immediately return None.
         return None
     # otherwise we return the *function* that can read ``path``.
     return reader_function
 
 
+def load_mat_vars(file_path: str) -> Dict:
+    """Load image variables from .mat file into dictionary
+
+    Args:
+        file_path (str): path to .mat file
+
+    Returns:
+        Dict: dictionary of image variables. Empty if file contains no images
+    """
+    # Load variable details before loading
+    mat_vars = sio.whosmat(file_path)
+    # Check if each variable is image based on shape
+    is_image_list = [shape_is_image(var[1]) for var in mat_vars]
+    var_list = [var[0] for var in mat_vars]
+    # Filter list of variables whether they are images
+    var_list = [i for (i, v) in zip(var_list, is_image_list) if v]
+    if len(var_list) > 0:
+        mat_dict = sio.loadmat(
+            file_path, variable_names=var_list, squeeze_me=True
+        )
+        for var in list(mat_dict.keys()):
+            if not hasattr(mat_dict[var], 'shape'):
+                del mat_dict[var]
+    else:
+        mat_dict = {}
+    return mat_dict
+
+
 def reader_function(path: PathLike) -> List[LayerData]:
     """Take a path or list of paths and return a list of LayerData tuples."""
     paths = [path] if isinstance(path, str) else path
-    # stack a list of strings, but also works for a single path string
-    data = np.squeeze(np.stack([imageio.imread(_path) for _path in paths]))
-    # Readers are expected to return data as a list of tuples, where each tuple
-    # is (data, [meta_dict, [layer_type]])
-    meta = {}  # optional kwargs for the corresponding viewer.add_* method
-    return [(data, meta)]
+
+    # Generate list to hold potential images from each path provided
+    data_list = [None for __ in range(len(paths))]
+    for i, _path in enumerate(paths):
+        try:
+            mat_dict = load_mat_vars(_path)
+            if not mat_dict:
+                continue
+            var_list = list(mat_dict.keys())
+            data = [None for __ in var_list]
+            for j, var in enumerate(var_list):
+                # optional kwargs for the corresponding viewer.add_* method
+                meta = {"name": var}
+                if len(mat_dict[var].shape) == 4:
+                    meta["channel_axis"] = 3
+                data[j] = (prep_array(mat_dict[var]), meta)
+            data_list[i] = data
+        except NotImplementedError:
+            # Scipy.io.loadmat does not support v7.3 files
+            print('v7.3 .mat files not supported.')
+
+    # Return None if no .mat files could be read
+    if all(value is None for value in data_list):
+        return None
+
+    # Flatten potential list of lists
+    data_list = [item for sublist in data_list for item in sublist]
+
+    return data_list
 
 
 def shape_is_image(shape: Sequence, min_size: int = 20) -> bool:
@@ -66,3 +103,38 @@ def shape_is_image(shape: Sequence, min_size: int = 20) -> bool:
     """
     dims = np.sum(np.array(shape) > min_size)
     return dims >= 2
+
+
+def prep_array(array: np.ndarray) -> np.ndarray:
+    """Correct images after loading to match Python
+
+    Args:
+        array (np.ndarray): array of at least two dimensions
+
+    Returns:
+        np.ndarray: Corrected array
+    """
+    # Boolean/logical arrays from Matlab are read as uint8
+    if array.dtype == "uint8":
+        if array.max() == 1:
+            array = array.astype("bool")
+
+    # Rearrange dimensions if 3D or higher
+    array = rearrange_dims(array)
+    return array
+
+
+def rearrange_dims(array: np.ndarray) -> np.ndarray:
+    """If image is more than 2D, move third dimension to first
+
+    Args:
+        array (np.ndarray): Multidimensional array
+
+    Returns:
+        np.ndarray: Array with rearranged axes
+    """
+    if len(array.shape) > 2:
+        # If third dimension is longer than first two, move to first position
+        if np.all(array.shape[2] > np.array(array.shape[0:2])):
+            array = np.moveaxis(array, 2, 0)
+    return array
--- tests/test_mat_images.py
+++ tests/test_mat_images.py
@@ -1,20 +1,44 @@
 # -*- coding: utf-8 -*-
 
-from napari_mat_images import napari_get_reader, shape_is_image
+from tempfile import NamedTemporaryFile
+
+import numpy as np
+import scipy.io as sio
+
+from napari_mat_images import (
+    napari_get_reader,
+    prep_array,
+    rearrange_dims,
+    shape_is_image,
+)
 
 
-# # the best test here would to use tempfile.NamedTemporaryFile
-# def test_reader():
-#     from tempfile import NamedTemporaryFile
-#     import numpy as np
-#
-#     # use your own `.ext` here
-#     with NamedTemporaryFile(suffix='.ext', delete=False) as tmp:
-#         out_data = np.random.rand(20, 20)
-#         # write_data_to_file(tmp.name, out_data)
-#         reader = napari_get_reader(tmp)
-#         in_data = reader(tmp.name)
-#         assert np.allclose(out_data, in_data)
+def test_reader():
+    with NamedTemporaryFile(suffix='.mat', delete=False) as tmp:
+        out_data = np.random.rand(25, 25)
+        sio.savemat(tmp.name, {"array": out_data})
+        reader = napari_get_reader(tmp.name)
+        in_data = reader(tmp.name)
+        assert np.allclose(out_data, in_data[0][0])
+
+
+def test_reader_channel_axis():
+    with NamedTemporaryFile(suffix='.mat', delete=False) as tmp:
+        out_data = np.random.rand(25, 25, 30, 3)
+        sio.savemat(tmp.name, {"array": out_data})
+        reader = napari_get_reader(tmp.name)
+        in_data = reader(tmp.name)
+        assert in_data[0][0].shape == (30, 25, 25, 3)
+        assert in_data[0][1]["channel_axis"] == 3
+
+
+def test_reader_no_images():
+    with NamedTemporaryFile(suffix='.mat', delete=False) as tmp:
+        out_data = "test string"
+        sio.savemat(tmp.name, {"string_value": out_data})
+        reader = napari_get_reader(tmp.name)
+        in_data = reader(tmp.name)
+        assert in_data is None
 
 
 def test_get_reader_hit():
@@ -44,3 +68,30 @@ def test_is_image():
 
     shape = [200, 1, 1]
     assert ~shape_is_image(shape)
+
+
+def test_rearrange_dims():
+    array = np.zeros((10, 10, 20))
+    array_updated = rearrange_dims(array)
+    assert array_updated.shape == (20, 10, 10)
+
+    array = np.zeros((20, 10, 10))
+    array_updated = rearrange_dims(array)
+    assert array_updated.shape == (20, 10, 10)
+
+    array = np.zeros((10, 10, 20, 4))
+    array_updated = rearrange_dims(array)
+    assert array_updated.shape == (20, 10, 10, 4)
+
+
+def test_prep_array_uint8():
+    array = np.random.randint(0, 255, (20, 20), dtype='uint8')
+    array_prepped = prep_array(array)
+    assert array_prepped.dtype == "uint8"
+
+
+def test_prep_array_bool():
+    array = np.zeros((20, 20), dtype='uint8')
+    array[0, 0] = 1
+    array_prepped = prep_array(array)
+    assert array_prepped.dtype == "bool"
